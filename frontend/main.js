00cd2c580aae49e1455081ea978b78d01193c959
divyansh0602@outlook.com <divyansh0602@outlook.com>
2025-11-21 18:01:14 +0530

    Updates to the structure, added main.js

---
+++ frontend/main.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount();
+});
+
+const functionAPI = '';
+
+const getVisitCount = () => {
+    let count = 20;
+    fetch(functionAPI).then(response => {
+        return response.json();
+    }).then(response => {
+        console.log("Function API is called by the site", response);
+        count = response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error) {
+        console.error(error);
+    });
+    return count;
+}
